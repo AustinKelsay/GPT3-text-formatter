683ad0909bd3838f7d6c416e49ea7000afb08968
austinkelsay <austinkelsay@yahoo.com>
2021-07-21 13:08:40 -0600

    function

--- single_example.py
+++ single_example.py
@@ -6,11 +6,13 @@ with open('secrets.json') as f:
   value = json.dumps(obj)
   token = value[16:59]
 
+text_prompt = "Watched Fight Club again recently, its interesting that the final mission and culmination of project mayhem is planting bombs to destroy the credit card/bank buildings totally erasing the debt record. Here we are in 2021 and we're racing towards this reality but it is our elites rather than a terrorist cult that's bringing this to fruition"
+
 openai.api_key = token
 
 response = openai.Completion.create(
   engine="davinci",
-  prompt="Division of labor. It is not immediately intuitive that specializing rather than generalizing would increase productivity, but because the fact that humans are innovators those that specialize also innovate their craft and leverage insight to increase their production. Therefore a society that specializes, advances significantly. Now in the scenario of specialization what is absolutely required is a unit of money or value or proof of work to allow exchange between all of the specialist individual‘s.",
+  prompt=text_prompt,
   temperature=0.3,
   max_tokens=60,
   top_p=1.0,
@@ -18,5 +20,5 @@ response = openai.Completion.create(
   presence_penalty=0.0
 )
 
-print(response)
+print(response.choices[0].text)
 
