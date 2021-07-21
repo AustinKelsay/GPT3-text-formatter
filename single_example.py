108a34e21f5e915f6619fc6d985dc9f6e6b92e04
austinkelsay <austinkelsay@yahoo.com>
2021-07-21 17:31:27 -0600

    Using env

--- single_example.py
+++ single_example.py
@@ -1,24 +1,25 @@
 import json
+from dotenv import load_dotenv
 import openai
+import os
 
-with open('secrets.json') as f:
-  obj = json.load(f)
-  value = json.dumps(obj)
-  token = value[16:59]
+load_dotenv()
+TOKEN = os.getenv("GPT3_TOKEN")
 
 text_prompt = "Watched Fight Club again recently, its interesting that the final mission and culmination of project mayhem is planting bombs to destroy the credit card/bank buildings totally erasing the debt record. Here we are in 2021 and we're racing towards this reality but it is our elites rather than a terrorist cult that's bringing this to fruition"
 
-openai.api_key = token
 
-response = openai.Completion.create(
-  engine="davinci",
-  prompt=text_prompt,
-  temperature=0.3,
-  max_tokens=60,
-  top_p=1.0,
-  frequency_penalty=0.0,
-  presence_penalty=0.0
-)
+def speak(text):
+  openai.api_key = TOKEN
+  response = openai.Completion.create(
+    engine="davinci",
+    prompt=text,
+    temperature=0.7,
+    max_tokens=100,
+    top_p=1.0,
+    frequency_penalty=0.0,
+    presence_penalty=0.0
+  )
+  return response.choices[0].text
 
-print(response.choices[0].text)
-
+print(speak(text_prompt))
