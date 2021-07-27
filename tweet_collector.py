d2b3d00edc2479ac9a46d08e7a4f36a1e8e033d3
austinkelsay <austinkelsay@yahoo.com>
2021-07-27 08:00:36 -0600

    Pulled first file of tweets

--- tweet_collector.py
+++ tweet_collector.py
@@ -1,10 +1,19 @@
 import requests
 import os
+import json
+from dotenv import load_dotenv
 
-TOKEN = os.getenv("TWITTER_TOKEN")
+load_dotenv()
+
+TOKEN = os.getenv("TOKEN")
 
 headers = {'Authorization': f"Bearer {TOKEN}"}
-params = {"exclude": 'retweets'}
+params = {"exclude": 'retweets',"exclude": "replies", "max_results": "100", "pagination_token": '7140dibdnow9c7btw3w4dhblnqvsmhd4ucvum49r9ge9d'}
 r = requests.get('https://api.twitter.com/2/users/745273/tweets', headers=headers , params=params)
 
-print(r.json())
+data = r.json()["data"]
+
+with open('tweets.json', 'w') as f:
+    for obj in data:
+        json.dump(obj, f)
+        f.write("\n")
